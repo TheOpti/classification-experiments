40bb609d02304f2d0533a36c7b3136f8d02d0f7e
TheOpti <wieckowski.piotr.93@gmail.com>
2018-04-08 19:25:08 +0200

    Add second plot for petal size

--- iris_data_visalization.py
+++ iris_data_visalization.py
@@ -10,30 +10,58 @@ param_cols = list(iris.columns.values)
 matrix = iris[param_cols].values
 labels = np.unique(iris['Species'])
 
-xcord1, ycord1 = [], []
-xcord2, ycord2 = [], []
-xcord3, ycord3 = [], []
+sepal_length_setosa, sepal_width_setosa = [], []
+sepal_length_versicolor, sepal_width_versicolor = [], []
+sepal_length_virginica, sepal_width_virginica = [], []
 
+petal_length_setosa, petal_width_setosa = [], []
+petal_length_versicolor, petal_width_versicolor = [], []
+petal_length_virginica, petal_width_virginica = [], []
+
+sepal_length_idx = 0
+sepal_width_idx = 1
 petal_length_idx = 2
 petal_width_idx = 3
 
 for n, elem in enumerate(matrix):
     if elem[-1] == 'Iris-setosa':
-        xcord1.append(matrix[n][petal_length_idx])
-        ycord1.append(matrix[n][petal_width_idx])
+        sepal_length_setosa.append(matrix[n][sepal_length_idx])
+        sepal_width_setosa.append(matrix[n][sepal_width_idx])
+        petal_length_setosa.append(matrix[n][petal_length_idx])
+        petal_width_setosa.append(matrix[n][petal_width_idx])
     elif elem[-1] == 'Iris-versicolor':
-        xcord2.append(matrix[n][petal_length_idx])
-        ycord2.append(matrix[n][petal_width_idx])
+        sepal_length_versicolor.append(matrix[n][sepal_length_idx])
+        sepal_width_versicolor.append(matrix[n][sepal_width_idx])
+        petal_length_versicolor.append(matrix[n][petal_length_idx])
+        petal_width_versicolor.append(matrix[n][petal_width_idx])
     elif elem[-1] == 'Iris-virginica':
-        xcord3.append(matrix[n][petal_length_idx])
-        ycord3.append(matrix[n][petal_width_idx])
+        sepal_length_virginica.append(matrix[n][sepal_length_idx])
+        sepal_width_virginica.append(matrix[n][sepal_width_idx])
+        petal_length_virginica.append(matrix[n][petal_length_idx])
+        petal_width_virginica.append(matrix[n][petal_width_idx])
 
-fig = plt.figure()
-ax = fig.add_subplot(111)
+sepal_fig = plt.figure()
+ax = sepal_fig.add_subplot(111)
 
-type1 = ax.scatter(xcord1, ycord1, s=50, c='red')
-type2 = ax.scatter(xcord2, ycord2, s=50, c='green')
-type3 = ax.scatter(xcord3, ycord3, s=50, c='blue')
+type1 = ax.scatter(sepal_length_setosa, sepal_width_setosa, s=50, c='red')
+type2 = ax.scatter(sepal_length_versicolor, sepal_width_versicolor, s=50, c='green')
+type3 = ax.scatter(sepal_length_virginica, sepal_width_virginica, s=50, c='blue')
+
+ax.set_title('Sepal size from Iris dataset', fontsize=14)
+ax.set_xlabel('Sepal length (cm)')
+ax.set_ylabel('Sepal width (cm)')
+ax.legend([type1, type2, type3], ["Iris Setosa", "Iris Versicolor", "Iris Virginica"], loc=2)
+
+ax.grid(True, linestyle='-', color='0.75')
+
+sepal_fig.savefig("./visualizations/iris_sepal.png", bbox_inches='tight')
+
+petal_fig = plt.figure()
+ax = petal_fig.add_subplot(111)
+
+type1 = ax.scatter(petal_length_setosa, petal_width_setosa, s=50, c='red')
+type2 = ax.scatter(petal_length_versicolor, petal_width_versicolor, s=50, c='green')
+type3 = ax.scatter(petal_length_virginica, petal_width_virginica, s=50, c='blue')
 
 ax.set_title('Petal size from Iris dataset', fontsize=14)
 ax.set_xlabel('Petal length (cm)')
@@ -42,7 +70,4 @@ ax.legend([type1, type2, type3], ["Iris Setosa", "Iris Versicolor", "Iris Virgin
 
 ax.grid(True, linestyle='-', color='0.75')
 
-fig.savefig("./visualizations/iris_petal_width_height.png", bbox_inches='tight')
-
-
-
+petal_fig.savefig("./visualizations/iris_petal.png", bbox_inches='tight')
