b68111223c92c76d4ac1052c826c1b960a973480
TheOpti <wieckowski.piotr.93@gmail.com>
2018-05-10 22:39:37 +0200

    Add classification methods for titanic dataset

--- titanic.py
+++ titanic.py
@@ -1,11 +1,12 @@
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from plot_utils import plot_learning_curve, plot_validation_curve, plot_confusion_matrix
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import AdaBoostClassifier
-from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.model_selection import GridSearchCV
 
 print 'Load data'
@@ -42,25 +43,56 @@ final_train_data = pd.get_dummies(train_data_2, columns=["embarked"])
 
 train_data_X = final_train_data[final_train_data.columns.difference(['survived'])]
 train_data_Y = final_train_data['survived']
+class_names=['Survived', 'Not survived']
 
 X_train, X_test, y_train, y_test = train_test_split(train_data_X, train_data_Y)
-
+'''
 tree_param_grid = {
     'criterion': ['gini', 'entropy'],
     'splitter': ['best', 'random'],
     'max_depth': np.arange(2, 8),
+    'min_samples_split': np.arange(2, 5),
+    'min_samples_leaf': [1, 2, 3]
 }
 
-tree = GridSearchCV(DecisionTreeClassifier(), tree_param_grid)
-
+tree = GridSearchCV(DecisionTreeClassifier(), tree_param_grid, verbose=1)
 tree.fit(X_train, y_train)
-tree_preds = tree.predict(X_test)
-tree_performance = accuracy_score(y_test, tree_preds)
+
+best = tree.best_estimator_
+best.fit(X_train, y_train)
+best_preds = best.predict(X_test)
+best_performance = accuracy_score(y_test, best_preds)
 
 print '\nDecision tree:'
 print 'Best params: ', tree.best_params_
-print 'Best scores: ', tree.best_score_
-print 'My score: ', tree_performance
+print 'Best score: ', tree.best_score_
+print 'My best score: ', best_performance
+
+print 'Classification report:'
+print(classification_report(y_test, best_preds, target_names=class_names))
+
+print 'Confusion matrix plotting...'
+cnf_matrix = confusion_matrix(y_test, best_preds)
+np.set_printoptions(precision=2)
+
+plt.figure()
+plot_confusion_matrix(cnf_matrix, classes=class_names, title='Confusion matrix')
+plt.savefig('./visualizations/titanic_tree_confusion_matrix.png')
+plt.clf()
+
+for param in tree_param_grid:
+    print 'Validation curve for ' + param + ' plotting...'
+    title = 'Decision tree - Validation curve (' + param + ')'
+    plot_validation_curve(best, title, train_data_X, train_data_Y, param, tree_param_grid[param])
+    plt.savefig('./visualizations/titanic_tree_validation_' + param + '.png')
+    plt.clf()
+
+print 'Learning curve plotting...'
+title = 'Decision tree - Learning curve'
+plot_learning_curve(tree, title, train_data_X, train_data_Y,)
+plt.savefig('./visualizations/titanic_tree_learning_curve.png')
+plt.clf()
+'''
 
 forest_param_grid = {
     'n_estimators': np.arange(10, 26, 2),
@@ -69,46 +101,83 @@ forest_param_grid = {
     'bootstrap': [True, False]
 }
 
-random_forest = GridSearchCV(RandomForestClassifier(), forest_param_grid)
-
+random_forest = GridSearchCV(RandomForestClassifier(), forest_param_grid, verbose=1)
 random_forest.fit(X_train, y_train)
-random_forest_preds = random_forest.predict(X_test)
-random_forest_performance = accuracy_score(y_test, random_forest_preds)
+
+best = random_forest.best_estimator_
+best.fit(X_train, y_train)
+best_preds = best.predict(X_test)
+best_performance = accuracy_score(y_test, best_preds)
 
 print '\nRandom forest:'
 print 'Best params: ', random_forest.best_params_
-print 'Best scores: ', random_forest.best_score_
-print 'My score: ', random_forest_performance
+print 'Best score: ', random_forest.best_score_
+print 'My best core: ', best_performance
+
+print 'Classification report:'
+print(classification_report(y_test, best_preds, target_names=class_names))
+
+print 'Confusion matrix plotting...'
+cnf_matrix = confusion_matrix(y_test, best_preds)
+np.set_printoptions(precision=2)
+
+plt.figure()
+plot_confusion_matrix(cnf_matrix, classes=class_names, title='Confusion matrix')
+plt.savefig('./visualizations/titanic_forest_confusion_matrix.png')
+plt.clf()
+
+for param in forest_param_grid:
+    print 'Validation curve for ' + param + ' plotting...'
+    title = 'Random forest - Validation curve (' + param + ')'
+    plot_validation_curve(best, title, train_data_X, train_data_Y, param, forest_param_grid[param])
+    plt.savefig('./visualizations/titanic_forest_validation_' + param + '.png')
+    plt.clf()
+
+print 'Learning curve plotting...'
+title = 'Random forest - Learning curve'
+plot_learning_curve(random_forest, title, train_data_X, train_data_Y)
+plt.savefig('./visualizations/titanic_forest_learning.png')
+plt.clf()
+
 
 ada_boost_param_grid = {
-    'base_estimator': [DecisionTreeClassifier()],
-    'n_estimators': np.arange(30, 200, 4)
+    'n_estimators': np.arange(12, 52, 4),
+    'learning_rate': [1, 1.5, 2.0, 2.5, 3.0]
 }
 
-ada_boost = GridSearchCV(AdaBoostClassifier(), ada_boost_param_grid)
-
+ada_boost = GridSearchCV(AdaBoostClassifier(), ada_boost_param_grid, verbose=1)
 ada_boost.fit(X_train, y_train)
-ada_boost_preds = ada_boost.predict(X_test)
-ada_boost_performance = accuracy_score(y_test, ada_boost_preds)
+
+best = ada_boost.best_estimator_
+best.fit(X_train, y_train)
+best_preds = best.predict(X_test)
+best_performance = accuracy_score(y_test, best_preds)
 
 print '\nAda Boost:'
 print 'Best params: ', ada_boost.best_params_
-print 'Best scores: ', ada_boost.best_score_
-print 'My score: ', ada_boost_performance
+print 'Best score: ', ada_boost.best_score_
+print 'My best core: ', best_performance
 
-gradient_boost_param_grid = {
-    'loss': ['deviance'],
-    'n_estimators': np.arange(50, 200, 4),
-    'criterion': ['friedman_mse', 'mse', 'mae']
-}
+print 'Classification report:'
+print(classification_report(y_test, best_preds, target_names=class_names))
 
-gradient_boost = GridSearchCV(GradientBoostingClassifier(), gradient_boost_param_grid)
+print 'Confusion matrix plotting...'
+cnf_matrix = confusion_matrix(y_test, best_preds)
+np.set_printoptions(precision=2)
 
-gradient_boost.fit(X_train, y_train)
-gradient_preds = gradient_boost.predict(X_test)
-gradient_performance = accuracy_score(y_test, gradient_preds)
+plt.figure()
+plot_confusion_matrix(cnf_matrix, classes=class_names, title='Confusion matrix')
+plt.savefig('./visualizations/titanic_ada_boost_confusion_matrix.png')
+plt.clf()
 
-print '\nGradientBoostingClassifier:'
-print 'Best params: ', gradient_boost.best_params_
-print 'Best scores: ', gradient_boost.best_score_
-print 'My score: ', gradient_performance
+for param in ada_boost_param_grid:
+    print 'Validation curve for ' + param + ' plotting...'
+    title = 'AdaBoost - Validation curve (' + param + ')'
+    plot_validation_curve(best, title, train_data_X, train_data_Y, param, ada_boost_param_grid[param])
+    plt.savefig('./visualizations/titanic_ada_boost_validation_' + param + '.png')
+    plt.clf()
+
+print 'Learning curve plotting...'
+title = 'Ada Boost - Learning Curves'
+plot_learning_curve(ada_boost, title, train_data_X, train_data_Y)
+plt.savefig('./visualizations/titanic_ada_boost_learning_curve.png')
