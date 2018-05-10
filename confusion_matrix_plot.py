4b689e091fa5636834914d06e3c484e249197f81
TheOpti <wieckowski.piotr.93@gmail.com>
2018-05-10 19:31:50 +0200

    Added confusion matrix plotting for iris dataset. Refactor

--- confusion_matrix_plot.py
+++ confusion_matrix_plot.py
@@ -1,10 +1,10 @@
-import itertools
 import numpy as np
 import matplotlib.pyplot as plt
 
 from sklearn import svm, datasets
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
+from plot_utils import plot_confusion_matrix
 
 # import some data to play with
 data = datasets.load_iris()
@@ -20,41 +20,6 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
 classifier = svm.SVC(kernel='linear', C=0.01)
 y_pred = classifier.fit(X_train, y_train).predict(X_test)
 
-
-def plot_confusion_matrix(cm, classes,
-                          normalize=False,
-                          title='Confusion matrix',
-                          cmap=plt.cm.Blues):
-    """
-    This function prints and plots the confusion matrix.
-    Normalization can be applied by setting `normalize=True`.
-    """
-    if normalize:
-        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-        print("Normalized confusion matrix")
-    else:
-        print('Confusion matrix, without normalization')
-
-    print(cm)
-
-    plt.imshow(cm, interpolation='nearest', cmap=cmap)
-    plt.title(title)
-    plt.colorbar()
-    tick_marks = np.arange(len(classes))
-    plt.xticks(tick_marks, classes)
-    plt.yticks(tick_marks, classes)
-
-    fmt = '.2f' if normalize else 'd'
-    thresh = cm.max() / 2.
-    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        plt.text(j, i, format(cm[i, j], fmt),
-                 horizontalalignment="center",
-                 color="white" if cm[i, j] > thresh else "black")
-
-
-    plt.ylabel('True label')
-    plt.xlabel('Predicted label')
-
 # Compute confusion matrix
 cnf_matrix = confusion_matrix(y_test, y_pred)
 np.set_printoptions(precision=2)
@@ -62,11 +27,11 @@ np.set_printoptions(precision=2)
 # Plot non-normalized confusion matrix
 plt.figure()
 plot_confusion_matrix(cnf_matrix, classes=class_names, title='Confusion matrix')
-plt.savefig('./visualizations/confusion_matrix.png')
+plt.savefig('./visualizations/example_confusion_matrix.png')
 plt.clf()
 
 # # Plot normalized confusion matrix
 plt.figure()
 plot_confusion_matrix(cnf_matrix, classes=class_names, normalize=True, title='Confusion matrix - normalized')
-plt.savefig('./visualizations/confusion_matrix_normalized.png')
+plt.savefig('./visualizations/example_confusion_matrix_normalized.png')
 plt.clf()
--- iris.py
+++ iris.py
@@ -1,25 +1,28 @@
 import numpy as np
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, classification_report
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import AdaBoostClassifier
-from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.svm import SVC
+from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import GridSearchCV
 import matplotlib.pyplot as plt
-from plot_learning_curve import plot_learning_curve, plot_validation_curve
+from plot_utils import plot_learning_curve, plot_validation_curve, plot_confusion_matrix
 
 print 'Load data'
 iris = load_iris()
 X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target)
+class_names = iris.target_names
 
 tree_param_grid = {
     'criterion': ['gini', 'entropy'],
     'splitter': ['best', 'random'],
     'max_depth': np.arange(2, 8),
     'min_samples_split': np.arange(2, 5),
-    'min_samples_leaf': [1, 2, 3]
+    'min_samples_leaf': [1, 2, 3],
+    'max_features': [None, 'auto', 'sqrt']
 }
 
 tree = GridSearchCV(DecisionTreeClassifier(), tree_param_grid, verbose=1)
@@ -36,7 +39,16 @@ print 'Best score: ', tree.best_score_
 print 'My best score: ', best_performance
 
 print 'Classification report:'
-print(classification_report(y_test, best_preds, target_names=iris.target_names))
+print(classification_report(y_test, best_preds, target_names=class_names))
+
+print 'Confusion matrix plotting...'
+cnf_matrix = confusion_matrix(y_test, best_preds)
+np.set_printoptions(precision=2)
+
+plt.figure()
+plot_confusion_matrix(cnf_matrix, classes=class_names, title='Confusion matrix')
+plt.savefig('./visualizations/iris_tree_confusion_matrix.png')
+plt.clf()
 
 print 'Learning curve plotting...'
 title = 'Decision tree - Learning curve'
@@ -56,7 +68,8 @@ forest_param_grid = {
     'n_estimators': np.arange(10, 26, 2),
     'criterion': ['gini', 'entropy'],
     'max_depth': np.arange(2, 8),
-    'bootstrap': [True, False]
+    'bootstrap': [True, False],
+    'max_features': [None, 'auto', 'sqrt']
 }
 
 random_forest = GridSearchCV(RandomForestClassifier(), forest_param_grid, verbose=1)
@@ -73,7 +86,16 @@ print 'Best score: ', random_forest.best_score_
 print 'My best core: ', best_performance
 
 print 'Classification report:'
-print(classification_report(y_test, best_preds, target_names=iris.target_names))
+print(classification_report(y_test, best_preds, target_names=class_names))
+
+print 'Confusion matrix plotting...'
+cnf_matrix = confusion_matrix(y_test, best_preds)
+np.set_printoptions(precision=2)
+
+plt.figure()
+plot_confusion_matrix(cnf_matrix, classes=class_names, title='Confusion matrix')
+plt.savefig('./visualizations/iris_forest_confusion_matrix.png')
+plt.clf()
 
 print 'Learning curve plotting...'
 title = 'Random forest - Learning curve'
@@ -88,10 +110,11 @@ for param in forest_param_grid:
     plt.savefig('./visualizations/iris_forest_validation_' + param + '.png')
     plt.clf()
 
-'''
+
 ada_boost_param_grid = {
-    'base_estimator': [DecisionTreeClassifier()],
-    'n_estimators': np.arange(30, 200, 4)
+    'base_estimator': [DecisionTreeClassifier(), GaussianNB(), SVC()],
+    'n_estimators': np.arange(30, 200, 4),
+    'learning_rate': [1, 1.4, 1.8, 2.0, 2.5, 3.0]
 }
 
 ada_boost = GridSearchCV(AdaBoostClassifier(), ada_boost_param_grid)
@@ -105,30 +128,26 @@ print 'Best params: ', ada_boost.best_params_
 print 'Best scores: ', ada_boost.best_score_
 print 'My score: ', ada_boost_performance
 
+print 'Classification report:'
+print(classification_report(y_test, best_preds, target_names=class_names))
+
+print 'Confusion matrix plotting...'
+cnf_matrix = confusion_matrix(y_test, best_preds)
+np.set_printoptions(precision=2)
+
+plt.figure()
+plot_confusion_matrix(cnf_matrix, classes=class_names, title='Confusion matrix')
+plt.savefig('./visualizations/iris_tree_confusion_matrix.png')
+plt.clf()
+
 print 'Plot learning curves...'
 title = 'Ada Boost - Learning Curves'
 plot_learning_curve(ada_boost, title, iris.data, iris.target)
 plt.savefig('./visualizations/learning_curve_iris_ada_boost.png')
 
-gradient_boost_param_grid = {
-    'loss': ['deviance'],
-    'n_estimators': np.arange(50, 200, 4),
-    'criterion': ['friedman_mse', 'mse', 'mae']
-}
-
-gradient_boost = GridSearchCV(GradientBoostingClassifier(), gradient_boost_param_grid)
-
-gradient_boost.fit(X_train, y_train)
-gradient_preds = gradient_boost.predict(X_test)
-gradient_performance = accuracy_score(y_test, gradient_preds)
-
-print '\nGradientBoostingClassifier:'
-print 'Best params: ', gradient_boost.best_params_
-print 'Best scores: ', gradient_boost.best_score_
-print 'My score: ', gradient_performance
-
-print 'Plot learning curves...'
-title = 'Gradient Boosting - Learning Curves'
-plot_learning_curve(gradient_boost, title, iris.data, iris.target)
-plt.savefig('./visualizations/learning_curve_iris_gradient_boosting.png')
-'''
+for param in ada_boost_param_grid:
+    print 'Validation curve for ' + param + ' plotting...'
+    title = 'Random forest - Validation curve (' + param + ')'
+    plot_validation_curve(best, title, iris.data, iris.target, param, forest_param_grid[param])
+    plt.savefig('./visualizations/iris_forest_validation_' + param + '.png')
+    plt.clf()
