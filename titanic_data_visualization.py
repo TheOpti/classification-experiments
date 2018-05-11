5ca7354fbe0c95a0e11114668ab9cd0603e26d49
TheOpti <wieckowski.piotr.93@gmail.com>
2018-05-11 16:10:41 +0200

    Add visualizations for Titanic dataset

--- titanic_data_visualization.py
+++ titanic_data_visualization.py
@@ -1,15 +1,45 @@
+import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt # data visualization
+import matplotlib.pyplot as plt
 import seaborn as sns
 
-train = pd.read_csv("./data/titanic/titanic.csv")
+titanic = pd.read_csv("./data/titanic/titanic.csv")
+
+titanic['travel_buds'] = titanic["sibsp"] + titanic["parch"]
+titanic['travel_alone'] = np.where(titanic['travel_buds'] > 0, 0, 1)
 
 plt.rc("font", size=14)
 sns.set(style="white")
 sns.set(style="whitegrid", color_codes=True)
 
-sns.countplot(x='embarked', data=train)
-plt.show()
+print 'Embarked'
+sns.countplot(x='embarked', data=titanic)
+plt.savefig('./visualizations/titanic_embarked.png')
+plt.clf()
 
-sns.barplot(x='sex', y='survived', data=train)
-plt.show()
+print 'Survived by sex'
+sns.barplot(x='sex', y='survived', data=titanic)
+plt.savefig('./visualizations/titanic_survived_by_sex.png')
+plt.clf()
+
+print 'Travel alone'
+sns.countplot(x='travel_alone', data=titanic)
+plt.savefig('./visualizations/titanic_travel_alone.png')
+plt.clf()
+
+print 'Class/sex ratio'
+sns.factorplot('pclass', hue='sex', data=titanic,  kind='count')
+plt.savefig('./visualizations/titanic_class_sex_ratio.png')
+plt.clf()
+
+print 'Class/survived ratio'
+g = sns.factorplot(x="sex", y="survived", col="pclass",
+                   data=titanic, saturation=.5,
+                   kind="bar", ci=None, aspect=.6)
+g.set_axis_labels("", "Survival Rate")\
+    .set_xticklabels(["Women", "Men"])\
+    .set_titles("{col_name} {col_var}")\
+    .set(ylim=(0, 1))\
+    .despine(left=True)
+plt.savefig('./visualizations/titanic_class_survived_ratio.png')
+plt.clf()
